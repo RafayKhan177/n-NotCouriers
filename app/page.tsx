e8fbb6ad43021698b9ef64a6aa2accedbdfd1e94
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-11-06 12:55:29 +0000

    bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import { TableSort, Header } from "../components/Index";
-import { HeroImage } from '../components/Index'
-
-=======
->>>>>>> Development
 export default function page() {
   return (
     <>
-      <HeroImage />
+      <p>Hi</p>
     </>
   );
 }
