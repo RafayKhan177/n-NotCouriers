ccd78ea373d1e807992a3f7969bab9d7236887d9
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-11-07 10:01:16 +0000

    bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,6 @@
 "use client"
 import { userRole } from "../api/firebase/functions/auth"
-
 export default function Page() {
-
-
-
   if (userRole() == null) return <p>Please log in</p>;
   return (
     <p>hi</p>
