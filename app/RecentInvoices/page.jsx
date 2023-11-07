ad7d7e9a470169dbd6464e7409668053d1cfe81f
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-11-07 09:24:16 +0000

    bug

--- app/RecentInvoices/page.jsx
+++ app/RecentInvoices/page.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { userRole } from "../../api/firebase/functions/auth";
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const [role, setRole] = useState(null);
-
   useEffect(() => {
     const userDoc = JSON.parse(localStorage.getItem("userDoc"));
     const userRoleValue = (userDoc || {}).role || null;
     setRole(userRoleValue);
   }, []);
-
   if (role === null) return <p>Please log in</p>;
-
   return (
     <p>Invoices</p>
   );
