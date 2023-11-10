3cd03ce0ebaa4bc970aec5b8afa0516bcd5a5719
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-11-10 19:10:41 +0000

    env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyBhY9LbIHmQUmjDsSfqYjRORMiiK133u1Y',
+    },
+    /* config options here */
+  };
+  
+  module.exports = nextConfig;
+  
