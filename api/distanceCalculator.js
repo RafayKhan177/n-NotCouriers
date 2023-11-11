a5cffb8cb3cb40a11b5a2d8767f89e69e689aad4
Rafay Khan <abdulrafaykhan857@gmail.com>
2023-11-11 00:26:04 +0000

    fix bug

--- api/distanceCalculator.js
+++ api/distanceCalculator.js
@@ -1,52 +1,17 @@
-const API_KEY = "AIzaSyBhY9LbIHmQUmjDsSfqYjRORMiiK133u1Y";
-
-const dummy = {
-    "destination_addresses": [
-        "151 Martins Ln, Tanwood VIC 3478, Australia"
-    ],
-    "origin_addresses": [
-        "110-140 King St, Sydney NSW 2000, Australia"
-    ],
-    "rows": [
-        {
-            "elements": [
-                {
-                    "distance": {
-                        "text": "583 mi",
-                        "value": 937964
-                    },
-                    "duration": {
-                        "text": "9 hours 49 mins",
-                        "value": 35360
-                    },
-                    "status": "OK"
-                }
-            ]
-        }
-    ],
-    "status": "OK"
-}
-
 async function calculateDistance(origin, destination) {
     try {
-        // const originStr = `${origin.lat},${origin.lng}`;
-        // const destinationStr = `${destination.lat},${destination.lng}`;
+        const originStr = `${origin.lat},${origin.lng}`;
+        const destinationStr = `${destination.lat},${destination.lng}`;
 
-        // const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${originStr}&destinations=${destinationStr}&key=${API_KEY}`;
-        // const res = await fetch(url);
+        const response = await fetch(`http://localhost:3000/distance?originStr=${originStr}&destinationStr=${destinationStr}`);
 
-        // if (!res.ok) {
-        //     // throw new Error("Failed to fetch data");
-        //     return url
-        // }
-
-        // const data = await res.json();
-        // return data;
-        return dummy
-
+        const data = await response.json();
+        console.log(data);
+        return data
     } catch (error) {
-        console.error('Error getting distance:', error);
+        console.error('Error fetching data:', error);
     }
+
 }
 
 export { calculateDistance };
