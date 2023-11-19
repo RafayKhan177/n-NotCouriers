3877ed33bcb71aabf66645bf600c194c76b0a354
RafayYousafzai1 <150551705+RafayYousafzai1@users.noreply.github.com>
2023-11-19 12:05:49 +0000

    bug

--- api/distanceCalculator.js
+++ api/distanceCalculator.js
@@ -2,14 +2,14 @@ async function calculateDistance(origin, destination) {
   try {
     const originStr = `${origin.lat},${origin.lng}`;
     const destinationStr = `${destination.lat},${destination.lng}`;
-
+    console.log(originStr, destinationStr);
     const response = await fetch(
-      `http://localhost:3000/?originStr=${originStr}&destinationStr=${destinationStr}`
+      `https://e-distance-matrix-8o2zubjue-rafaykhan177.vercel.app/api/distance?originStr=${originStr}&destinationStr=${destinationStr}`
     );
 
     const data = await response.json();
     console.log(data);
-    return data;
+    return data.distanceData;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
